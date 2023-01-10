1c3559cb64942a58895a48699553913a198bbc9e
Saif Adel <saifadel670@gmail.com>
2023-01-10 18:09:37 +0600

    Time: 28 ms (74.60%), Space: 13.9 MB (73.79%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func intToRoman(_ num: Int) -> String {
+        var integerValue = num
+        var numeralString = ""
+        let mappingList: [(Int, String)] = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+        for i in mappingList {
+            while (integerValue >= i.0) {
+                integerValue -= i.0
+                numeralString += i.1
+            }
+        }
+        return numeralString
+        
+    }
+    
+
+
+}
